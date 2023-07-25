de71697fa97b25ec2a7364c5a2d08a3e7649c63b
suwumonster <20212121013@m.scnu.edu.cn>
2023-07-25 16:32:12 +0800

    Update

--- Leetcode/Leetcode26.c
+++ Leetcode/Leetcode26.c
@@ -1,19 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//*********************************
+// 作者：suwu
+// 环境: vs2019 win10
+// 最后修改时间：2023/7/25 16:29
+//*********************************
+
+//思路一 双指针 类似LC27
+
+// 函数名称：removeDuplicates
+// 函数功能：删除有序数组中的重复数据，返回去重后的元素个数
+// 函数参数：数组指针，元素个数
+// 返回类型及内容：int，去重后的元素个数
 int removeDuplicates(int* nums, int numsSize) {
-    int i = 0;
-    int j = 1;
-    int num = numsSize;
-    while (j < numsSize)
+    int src = 0;
+    int dst = 0;
+
+    while (src < numsSize)
     {
-        if (nums[i] != nums[j])
+        if (nums[src] == nums[dst])
         {
-            nums[i + 1] = nums[j];
-            i++;
-            j++;
-            continue;
+            src++;
         }
-        j++;
-        num--;
+        else
+        {
+            dst++;
+            nums[dst] = nums[src];
+        }
     }
-    return num;
+    return dst + 1;
 }
