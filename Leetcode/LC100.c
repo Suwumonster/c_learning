795caa4ed924914827c68a82044b60843fc06fa7
suwumonster <20212121013@m.scnu.edu.cn>
2023-10-24 14:58:53 +0800

    commit code

---
+++ Leetcode/LC100.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+
+    if (p == NULL || q == NULL)
+    {
+        return false;
+    }
+
+    if (p->val != q->val)
+    {
+        return false;
+    }
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
